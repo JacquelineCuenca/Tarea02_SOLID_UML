37af1e0904099ed4568e4625d8760e506bbaed36
AxelRoman1 <150213728+AxelRoman1@users.noreply.github.com>
2025-06-22 22:35:59 -0500

    Update PagoService.java

--- Tarea Principios Solid y UML_GRUPO8/tarea_solid_uml/src/main/java/com/example/PagoService.java
+++ Tarea Principios Solid y UML_GRUPO8/tarea_solid_uml/src/main/java/com/example/PagoService.java
@@ -1,8 +1,1 @@
 package com.example;
-
-public class PagoService {
-    public boolean procesarPago(double monto) {
-        System.out.println("Procesando pago: $" + monto);
-        return true;
-    }
-}
